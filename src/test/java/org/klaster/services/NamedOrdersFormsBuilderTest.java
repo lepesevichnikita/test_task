7a5469475277789b245f066430c4d7470c755580
Nikita Lepesevich <lepesevich.nikita@yandex.ru>
2019-09-25 18:15:46 +0300

    Add NamedOrdersFormsBuilderTest for NamedOrdersFormsBuilder testing Add NamedOrdersSuffixesBuilderTest for NamedOrdersSuffixesBuilder testing
    Signed-off-by: Nikita Lepesevich <lepesevich.nikita@yandex.ru>

---
+++ src/test/java/org/klaster/services/NamedOrdersFormsBuilderTest.java
@@ -0,0 +1,65 @@
+package org.klaster.services;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.klaster.models.NamedOrder;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Nikita Lepesevich <lepesevich.nikita@yandex.ru> on 9/25/19
+ * @project testtask
+ */
+
+public class NamedOrdersFormsBuilderTest {
+    private static NamedOrdersFormsBuilder namedOrdersFormsBuilder;
+
+    private static Stream<Arguments> namedOrders() {
+        return Stream.of(
+                Arguments.of(2, "миллион", NamedOrder.Gender.MASCULINE, "миллион", "миллиона", "миллионы", "миллионов"),
+                Arguments.of(1, "тысяч", NamedOrder.Gender.FEMININE, "тысяча", "тысячи", "тысячи", "тысяч"),
+                Arguments.of(0, "", NamedOrder.Gender.MASCULINE, "", "", "", "")
+        );
+    }
+
+    @BeforeAll
+    static void init() {
+        namedOrdersFormsBuilder = new NamedOrdersFormsBuilder();
+    }
+
+    @AfterEach
+    void reset() {
+        namedOrdersFormsBuilder.reset();
+    }
+
+    @ParameterizedTest
+    @DisplayName("Builds correct forms of named order, considering it gender")
+    @MethodSource("namedOrders")
+    void buildsNamedOrdersForms(int namedOrderNumber, String root, NamedOrder.Gender gender,
+                                String expectedSingularNominative, String expectedSingularGenitive,
+                                String expectedPluralNominative,
+                                String expectedPluralGenitive) {
+        NamedOrder namedOrder = new NamedOrder();
+        namedOrder.setNamedOrderNumber(namedOrderNumber);
+        namedOrder.setRoot(root);
+        namedOrder.setGender(gender);
+
+        namedOrdersFormsBuilder.withNamedOrder(namedOrder);
+        assertEquals(expectedSingularNominative, namedOrdersFormsBuilder.getResult());
+
+        namedOrdersFormsBuilder.withCase(NamedOrder.Case.GENITIVE);
+        assertEquals(expectedSingularGenitive, namedOrdersFormsBuilder.getResult());
+
+        namedOrdersFormsBuilder.withForm(NamedOrder.Form.PLURAL).withCase(NamedOrder.Case.NOMINATIVE);
+        assertEquals(expectedPluralNominative, namedOrdersFormsBuilder.getResult());
+
+        namedOrdersFormsBuilder.withCase(NamedOrder.Case.GENITIVE);
+        assertEquals(expectedPluralGenitive, namedOrdersFormsBuilder.getResult());
+    }
+}
---
+++ src/test/java/org/klaster/services/NamedOrdersSuffixesBuilderTest.java
@@ -0,0 +1,50 @@
+package org.klaster.services;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.klaster.models.NamedOrder;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class NamedOrdersSuffixesBuilderTest {
+    private static NamedOrdersSuffixesBuilder namedOrdersSuffixesBuilder;
+
+    private static Stream<Arguments> suffixParams() {
+        return Stream.of(
+                Arguments.of(NamedOrder.Gender.MASCULINE, NamedOrder.Form.SINGULAR, NamedOrder.Case.NOMINATIVE, ""),
+                Arguments.of(NamedOrder.Gender.MASCULINE, NamedOrder.Form.SINGULAR, NamedOrder.Case.GENITIVE, "а"),
+                Arguments.of(NamedOrder.Gender.MASCULINE, NamedOrder.Form.PLURAL, NamedOrder.Case.NOMINATIVE, "ы"),
+                Arguments.of(NamedOrder.Gender.MASCULINE, NamedOrder.Form.PLURAL, NamedOrder.Case.GENITIVE, "ов"),
+                Arguments.of(NamedOrder.Gender.FEMININE, NamedOrder.Form.SINGULAR, NamedOrder.Case.NOMINATIVE, "а"),
+                Arguments.of(NamedOrder.Gender.FEMININE, NamedOrder.Form.SINGULAR, NamedOrder.Case.GENITIVE, "и"),
+                Arguments.of(NamedOrder.Gender.FEMININE, NamedOrder.Form.PLURAL, NamedOrder.Case.NOMINATIVE, "и"),
+                Arguments.of(NamedOrder.Gender.FEMININE, NamedOrder.Form.PLURAL, NamedOrder.Case.GENITIVE, "")
+        );
+    }
+
+    @BeforeAll
+    static void init() {
+        namedOrdersSuffixesBuilder = new NamedOrdersSuffixesBuilder();
+    }
+
+    @AfterEach
+    void reset() {
+        namedOrdersSuffixesBuilder.reset();
+    }
+
+    @ParameterizedTest
+    @DisplayName("Builds valid suffixes by given params")
+    @MethodSource("suffixParams")
+    void buildsSuffixes(NamedOrder.Gender requiredGender, NamedOrder.Form requiredForm, NamedOrder.Case requiredCase,
+                        String expectedSuffixValue) {
+        namedOrdersSuffixesBuilder.withGender(requiredGender).withForm(requiredForm).withCase(requiredCase);
+        assertEquals(expectedSuffixValue, namedOrdersSuffixesBuilder.getResult());
+    }
+
+}
