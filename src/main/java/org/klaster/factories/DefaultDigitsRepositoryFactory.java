494739f00f685a8bbf9312143245f5eaf901177b
Nikita Lepesevich <lepesevich.nikita@yandex.ru>
2019-09-30 03:11:52 +0300

    Extract interface of DefaultDigitsRepositoryFactory and replace all usages of it with this interface
    Signed-off-by: Nikita Lepesevich <lepesevich.nikita@yandex.ru>

--- src/main/java/org/klaster/factories/DefaultDigitsRepositoryFactory.java
+++ src/main/java/org/klaster/factories/DefaultDigitsRepositoryFactory.java
@@ -1,14 +1,21 @@
 package org.klaster.factories;
 
 import org.klaster.interfaces.DigitsRepositoryFactory;
+import org.klaster.services.DefaultDigitsRepository;
 
 /**
  * @author Nikita Lepesevich <lepesevich.nikita@yandex.ru> on 9/28/19
  * @project testtask
  */
 public class DefaultDigitsRepositoryFactory implements DigitsRepositoryFactory {
+
     @Override
     public String getDictionaryName() {
         return "digits.yaml";
+    }
+
+    @Override
+    public Class getRepositoryClass() {
+        return DefaultDigitsRepository.class;
     }
 }
--- src/main/java/org/klaster/interfaces/DigitsRepositoryFactory.java
+++ src/main/java/org/klaster/interfaces/DigitsRepositoryFactory.java
@@ -1,15 +1,8 @@
 package org.klaster.interfaces;
 
-import org.klaster.services.DefaultDigitsRepository;
-
 /**
  * @author Nikita Lepesevich <lepesevich.nikita@yandex.ru> on 9/28/19
  * @project testtask
  */
 public interface DigitsRepositoryFactory extends RepositoryFactory<DigitsRepository> {
-
-    @Override
-    default Class<?> getRepositoryClass() {
-        return DefaultDigitsRepository.class;
-    }
 }
