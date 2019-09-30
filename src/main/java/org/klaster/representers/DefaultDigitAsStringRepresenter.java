3690bd505d7e2fc348ecc8b0737fd681c04a2e79
Nikita Lepesevich <lepesevich.nikita@yandex.ru>
2019-09-30 03:23:31 +0300

    Rename DigitsGenderFormsBuilder to DefaultDigitAsStringRepresenter Extract interface of DefaultDigitAsStringRepersenter and replace all usages of it with this interface
    Signed-off-by: Nikita Lepesevich <lepesevich.nikita@yandex.ru>

--- src/main/java/org/klaster/representers/DefaultDigitAsStringRepresenter.java
+++ src/main/java/org/klaster/representers/DefaultDigitAsStringRepresenter.java
@@ -1,5 +1,6 @@
-package org.klaster.builders;
+package org.klaster.representers;
 
+import org.klaster.interfaces.DigitAsStringRepresenter;
 import org.klaster.models.Declension;
 import org.klaster.models.Digit;
 
@@ -7,30 +8,29 @@ import org.klaster.models.Digit;
  * @author Nikita Lepesevich <lepesevich.nikita@yandex.ru> on 9/25/19
  * @project testtask
  */
-public class DigitsGenderFormsBuilder {
+public class DefaultDigitAsStringRepresenter implements DigitAsStringRepresenter {
     private Digit             digit;
     private Declension.Gender gender;
 
-    public DigitsGenderFormsBuilder() {
-        reset();
-    }
-
+    @Override
     public void reset() {
         digit  = null;
         gender = Declension.Gender.MASCULINE;
     }
 
-    public DigitsGenderFormsBuilder withDigit(Digit digit) {
+    @Override
+    public String from(Digit digit) {
         this.digit = digit;
-        return this;
+        return getResult();
     }
 
-    public DigitsGenderFormsBuilder withGender(Declension.Gender gender) {
+    @Override
+    public DigitAsStringRepresenter withGender(Declension.Gender gender) {
         this.gender = gender;
         return this;
     }
 
-    public String getResult() {
+    private String getResult() {
         assert(digit != null);
         String result = digit.getGenderForm(gender);
         if (result.isEmpty() && gender != Declension.Gender.MASCULINE) {
