c45b21cd4b0379e5ca8702c3ad60bc25d29d6b37
Nikita Lepesevich <lepesevich.nikita@yandex.ru>
2019-09-30 03:13:24 +0300

    Export interface of DefaultTripleSequenceFactoryBuilder and replace all usages of it to this interface
    Signed-off-by: Nikita Lepesevich <lepesevich.nikita@yandex.ru>

--- src/main/java/org/klaster/interfaces/NamedOrdersRepositoryFactory.java
+++ src/main/java/org/klaster/interfaces/NamedOrdersRepositoryFactory.java
@@ -1,15 +1,8 @@
 package org.klaster.interfaces;
 
-import org.klaster.services.DefaultNamedOrdersRepository;
-
 /**
  * @author Nikita Lepesevich <lepesevich.nikita@yandex.ru> on 9/28/19
  * @project testtask
  */
 public interface NamedOrdersRepositoryFactory extends RepositoryFactory<NamedOrdersRepository> {
-
-    @Override
-    default Class<?> getRepositoryClass() {
-        return DefaultNamedOrdersRepository.class;
-    }
 }
